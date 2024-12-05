1c87b4b6b74c25d122b575cb4cf0adfd888501e2
DanielTrujillo5 <danieltigo555@gmail.com>
2024-12-04 22:15:37 -0500

    feat:ll

--- colegio/src/app/app.component.ts
+++ colegio/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConsultaComponent } from './components/consulta/consulta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ConsultaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
