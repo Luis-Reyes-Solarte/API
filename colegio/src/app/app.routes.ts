a16a54fb4416ec5a2fe4c4855264a2be700c28e6
DanielTrujillo5 <danieltigo555@gmail.com>
2024-12-04 21:35:42 -0500

    feat:dos

--- colegio/src/app/app.routes.ts
+++ colegio/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { ConsultaComponent } from './components/consulta/consulta.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+
+        path: 'cosulta',
+        component: ConsultaComponent
+    
+    },
+
+    {
+
+        path: 'listado',
+        component: ListadoComponent
+    
+    },
+    {
+
+        path: 'registro',
+        component: RegistroComponent
+    
+    },
+];
