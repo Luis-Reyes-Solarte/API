2332968582049cb2d9d8c9035170c572f9c30520
DanielTrujillo5 <danieltigo555@gmail.com>
2024-12-05 00:09:38 -0500

    feat:ya

--- colegio/src/app/components/listado/listado.component.ts
+++ colegio/src/app/components/listado/listado.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { UserserviceService } from '../../services/userservice.service';  // Asegúrate de que este sea el servicio correcto
 
 @Component({
   selector: 'app-listado',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, HttpClientModule, CommonModule, TableModule],
   templateUrl: './listado.component.html',
+  providers: [ UserserviceService ],
   styleUrl: './listado.component.css'
 })
 export class ListadoComponent {
+  users: any[] = [];  // Almacena los usuarios.
+  errorMessage: string = '';  // Mensaje de error si la carga falla.
 
+  constructor(private userservice: UserserviceService) {
+    this.loadUsers();  // Cargar usuarios al iniciar el componente
+  }
+
+  loadUsers(): void {
+    this.userservice.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;  // Asignar los usuarios a la variable
+      },
+      error: () => {
+        this.errorMessage = 'Error al cargar los usuarios';  // Mostrar un mensaje de error
+      }
+    });
+  }
 }
