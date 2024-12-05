73908972f12230fda79be89698dd71f8d54dcd6d
DanielTrujillo5 <danieltigo555@gmail.com>
2024-12-04 22:07:18 -0500

    feat:tres

--- colegio/src/app/components/consulta/consulta.component.ts
+++ colegio/src/app/components/consulta/consulta.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
-
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserserviceService } from '../../services/userservice.service';
 @Component({
   selector: 'app-consulta',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ButtonModule, InputTextModule,HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.css'
 })
 export class ConsultaComponent {
+
+ estudiante: any[] = []; // Almacena los usuarios filtrados.
+  codigo: string = ''; // ID ingresado por el usuario.
+  errorMessage: string = '';
+  isLoading: boolean = false; // Indica si la búsqueda está en progreso.
+
+  constructor(private UserserviceService: UserserviceService) {}
+
+  // Método para buscar usuarios por ID
+  searchUserById(): void {
+    if (!this.codigo) {
+      this.errorMessage = 'Por favor, ingresa un codigo válido.';
+      return;
+    }
+  
+    this.isLoading = true;
+    this.UserserviceService.getUserById(this.codigo).subscribe({
+      next: (user) => {
+        this.estudiante = user ? [user] : [];
+        this.errorMessage = this.estudiante.length ? '' : 'No se encontró ningún estudiante con este codigo.';
+      },
+      error: () => {
+        this.errorMessage = 'Error al buscar el estudiante.';
+        this.estudiante = [];
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
 
 }
--- colegio/src/app/services/userservice.service.ts
+++ colegio/src/app/services/userservice.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserserviceService {
+ //Url del API almacenada de forma privada
+ apiUrl= "https://localhost:7059/api/Users";
 
-  constructor() { }
+ constructor(private http: HttpClient) { }
+
+ // Observable es un objeto que representa un flujo de datos que se pueden manejar 
+ // de forma asíncrona. Los Observables son parte de la biblioteca RxJS (Reactive Extensions for JavaScript) 
+ // y se utilizan principalmente para manejar eventos o flujos de datos que pueden ocurrir en el futuro, 
+ // como respuestas de peticiones HTTP, eventos del usuario - se controla con HttpClientModule
+
+ login(username: string, password:string):Observable<any>{
+   const body = { username, password };
+   return this.http.post(`${this.apiUrl}/login`,{username, password});
+
+   //return this.http.post('${this.apiUrl}/login', {username, password})
+
+ }
+
+ register(username:string, password:string):Observable<any> {
+   const body = { username, password };
+   return this.http.post(`${this.apiUrl}/register`,{username, password});
+ }
+
+ deleteUser (id: string): Observable<any> {
+   return this.http.delete(`${this.apiUrl}/delete/${id}`);
+ }
+
+ update(Id: string, username: string, password: string): Observable<any> {
+   const body = { username, password };
+   return this.http.put(`${this.apiUrl}/update/${Id}`, body);
+}
+
+ 
+getUsers (): Observable<any[]> {
+ return this.http.get<any[]>(`${this.apiUrl}/getusers`);
+}
+//Obtener usuario por su Id
+getUserById(userId: string): Observable<any> {
+ return this.http.get<any>(`${this.apiUrl}/getUserById/${userId}`);
+}
 }
