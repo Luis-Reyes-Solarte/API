8b3d68e15af35067b2ed35601d5f7460145d4bb4
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-12-04 21:45:06 -0500

    feat: registro

--- colegio/src/app/components/registro/registro.component.ts
+++ colegio/src/app/components/registro/registro.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule,FormGroup,FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
 
+  registroForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.registroForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['',[Validators.required, Validators.minLength(6)]]
+    })
+  };
 }
