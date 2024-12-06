6a34deaad2eb415d48728dc2aecca9e73071d1ad
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-12-05 21:58:12 -0500

    feat: register httpost

--- colegio/Api/Apicolegio/Controllers/colegioController.cs
+++ colegio/Api/Apicolegio/Controllers/colegioController.cs
@@ -5,9 +5,35 @@ using Dapper;
 
 namespace colegio.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    private readonly string _connectionString = "Server=LuisReyes\\MSSQLSERVER01;Database=dbform;User Id=sa;Password=12345678;TrustServerCertificate=true";
+
+    [HttpPost("register")]
+    public IActionResult Register([FromBody] Users user)
     {
+      if (user == null)
+      {
+        return BadRequest("Invalid user data.");
+      }
+
+      using (var connection = new SqlConnection(_connectionString))
+      {
+        var sql = "INSERT INTO Users (Username, Password) VALUES (@Username, @Password)";
+        var rowsAffected = connection.Execute(sql, new { user.username, user.cedula, user.codigo });
+
+        if (rowsAffected > 0)
+        {
+          return Ok("User registered successfully.");
+        }
+        else
+        {
+          return StatusCode(500, "An error occurred while registering the user.");
+        }
+      }
+
     }
+  }
 }
